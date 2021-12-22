97ba192fe36a933eeabbdbe388bad81e986c9ce1
Facu DLF <facundo.delafuente@radiumrocket.com>
2021-12-22 16:30:05 -0300

    Fix reset buttons and styling.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/RadiumRockletsCinema/' //nombre de tu proyecto GitHub
-    : '/'
-}
+    publicPath: '/RadiumRockletsCinema',
+  }
